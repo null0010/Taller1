f74581a2077fc1f5071a8308dc087c7b8f76c66b
NullCoder <90267124+null0010@users.noreply.github.com>
2021-11-05 06:38:39 +0900

    Update App.java

--- src/cl/ucn/ei/pa/sistemaRitoGames/logica/App.java
+++ src/cl/ucn/ei/pa/sistemaRitoGames/logica/App.java
@@ -2,6 +2,13 @@ package cl.ucn.ei.pa.sistemaRitoGames.logica;
 
 public class App {
     public static void main(String[] args) {
+        SistemaRitoGames sistemaRitoGames = new SistemaRitoGamesImpl();
+
+
+    }
+
+
+    public static void cargarPersonajes(SistemaRitoGames sistemaRitoGames) {
 
     }
 }
